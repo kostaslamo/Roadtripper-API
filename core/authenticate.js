75b257fe088ca9abb40d7b06cec35066e7b68f0f
Kostas Lamogiannnis <kostaslamo91@gmail.com>
2019-04-25 13:05:53 +0300

    Getting emails and passwords from db to match authentication - simplev1

--- core/authenticate.js
+++ core/authenticate.js
@@ -6,25 +6,29 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
   const { email, password } = req.body;
-  if (email && email === 'kostaslamo91@gmail.com') {
-    if (password && password === 'kostaslamo91') {
-      //if eveything is okey let's create our token 
-      const payload = {
-        check:  true
-      };
-      const token = jwt.sign(payload, config.secret, {
-        expiresIn: 1440 // expires in 24 hours
-      });
-      res.json({
-        message: 'Authenticated',
-        token: token
-      });
+  DB.query('select email,password_digest from users').then((userData) => {
+    const data = userData[0];
+    const emails = data.map(d => d.email);
+    const passwords = data.map(d => d.password_digest);
+    if (email && emails.indexOf(email) !== -1) {
+      if (password && passwords.indexOf(password) !== -1) {
+        const payload = {
+          check:  true
+        };
+        const token = jwt.sign(payload, config.secret, {
+          expiresIn: 1440 // expires in 24 hours
+        });
+        res.json({
+          message: 'Authenticated',
+          token: token
+        });
+      } else {
+        res.json({ message: "please check your password !" });
+      }
     } else {
-      res.json({ message: "please check your password !" });
+      res.json({message:"user not found !"})
     }
-  } else{
-    res.json({message:"user not found !"})
-  }
+  });
 });
 
 module.exports = router;
