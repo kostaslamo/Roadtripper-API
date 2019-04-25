08fd0fe30d2191c305eb1f8d364b7342d08bd6bd
Kostas Lamogiannnis <kostaslamo91@gmail.com>
2019-04-25 12:52:13 +0300

    DB configurations to config.js

--- configurations/config.js
+++ configurations/config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  secret : process.env.API_SECRET
+  secret : process.env.API_SECRET,
+  rds : {
+    dialeg: 'postgres',
+    host: process.env.RDS_HOST,
+    port: process.env.RDS_PORT || 5432,
+    db: process.env.RDS_DB,
+    user: process.env.RDS_USER,
+    pass: process.env.RDS_PASS,
+  },
 }
--- db/index.js
+++ db/index.js
@@ -1,31 +1,12 @@
 const Sequelize = require('sequelize');
-
-const rds = {
-  host: process.env.RDS_HOST,
-  port: process.env.RDS_PORT || 5432,
-  db: process.env.RDS_DB,
-  user: process.env.RDS_USER,
-  pass: process.env.RDS_PASS,
-};
-
-// const init = () => new Promise((resolve, reject) => {
-//   massive({
-//     host: rds.host,
-//     port: rds.port,
-//     database: rds.db,
-//     user: rds.user,
-//     password: rds.pass,
-//   }).then(resolve)
-//     .catch(reject);
-// });
+const config = require('../configurations/config');
 
 const init = () => {
-  global.DB = new Sequelize(rds.db, rds.user, rds.pass, {
-    host: rds.host,
-    dialect: 'postgres'
+  global.DB = new Sequelize(config.rds.db, config.rds.user, config.rds.pass, {
+    host: config.rds.host,
+    dialect: config.rds.dialeg,
   });
 }
-
 
 module.exports = {
   init,
