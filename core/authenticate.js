f1f86c7aa84ec7f329cc4a44fe3e0bfd81039b3c
Kostas Lamogiannnis <kostaslamo91@gmail.com>
2019-04-25 13:25:31 +0300

    Using hashed passwords

--- core/authenticate.js
+++ core/authenticate.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const config = require('../configurations/config');
+const { encrypt, decrypt } = require('../services/encryptDecrypt');
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
   const { email, password } = req.body;
-  DB.query('select email,password_digest from users').then((userData) => {
+  DB.query('select email, password_digest from users').then((userData) => {
     const data = userData[0];
     const emails = data.map(d => d.email);
-    const passwords = data.map(d => d.password_digest);
+    const passwords = data.map(d => decrypt(d.password_digest));
     if (email && emails.indexOf(email) !== -1) {
       if (password && passwords.indexOf(password) !== -1) {
         const payload = {
---
+++ services/encryptDecrypt.js
@@ -0,0 +1,28 @@
+const crypto = require('crypto');
+
+const ENCRYPTION_KEY = process.env.ENCRYPTION_TEXT_KEY; // Must be 256 bytes (32 characters)
+const IV_LENGTH = 16; // For AES, this is always 16
+
+const encrypt = (text) => {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv('aes-256-cbc', new Buffer(ENCRYPTION_KEY), iv);
+  let encrypted = cipher.update(text);
+
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+  return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+};
+
+const decrypt = (text) => {
+  const textParts = text.split(':');
+  const iv = new Buffer(textParts.shift(), 'hex');
+  const encryptedText = new Buffer(textParts.join(':'), 'hex');
+  const decipher = crypto.createDecipheriv('aes-256-cbc', new Buffer(ENCRYPTION_KEY), iv);
+  let decrypted = decipher.update(encryptedText);
+
+ decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+ return decrypted.toString();
+}
+
+module.exports = { encrypt, decrypt };
